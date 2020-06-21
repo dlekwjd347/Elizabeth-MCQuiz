c132f31f0a79c9f99d3e4341f074dd906a4e0958
Elizabeth Lee <dlekwjd347@gmail.com>
2020-06-21 16:29:48 -0400

    progress 6.21.20 - start page

---
+++ script.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        prompt: "Knowing correct grammar will help you write ________.\n(a) good\n(b) well\n (c) goodly\n(d)clear",
+        answer: "b"
+    }
+
+
+
+
+
+
+
+
+]
+
+var score = 0;
+
+for (var i = 0; i < questions.length; i++) {
+    var response = window.prompt()
+
+    if (response === questions[i].answer) {
+        score++;
+    }
+
+    else {
+        timer--;
+
+    }
+}
