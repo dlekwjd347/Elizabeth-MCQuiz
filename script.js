1e2db41e39682fa0c9cd2098f20d5343ac9284fe
Elizabeth Lee <dlekwjd347@gmail.com>
2020-06-21 16:49:30 -0400

    animate added to h1 tag

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+p.setAttribute("class", "animate__animated animate__zoomInDown animate__infinite");
+
 var questions = [
     {
         prompt: "Knowing correct grammar will help you write ________.\n(a) good\n(b) well\n (c) goodly\n(d)clear",
