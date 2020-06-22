f7da93f0ef15210b80938f68eb42549051ce3a69
Elizabeth Lee <dlekwjd347@gmail.com>
2020-06-21 21:42:49 -0400

    started timer when Start Quiz button is clicked

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 var score = 0;
+var timeEl = document.querySelector(".timeLeft");
 var questionsEl = document.querySelector("questions");
 var startBtn = document.getElementById("start");
+
+var secondsLeft = 50;
 
 function startButton() {
     var startscreenEl = document.querySelector(".start-screen");
     //when start button is clicked, hide start screen
     startscreenEl.setAttribute("style", "display: none;");
+    //when start button is clicked, timer starts
+    
+    // setTimeout(function(){
+    //     initializeQuestion();
+        
+    // }, 1000);
 
-    //un-hide questions
-    // questionsEl.removeAttribute("class");
+}
 
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = `Time Remaining: ${secondsLeft} seconds`; 
+        // score.textContent= "";
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            // alert("Time's up!")
+        }
+    }, 1000);
 }
 
 document.addEventListener("click", function (event) {
     if (event.target.matches("#start")) {
-        startButton()
-
+        startButton();
+        setTime();
     }
-
-
     //stops function to end it (retire function)
     return
 })
+
+function initializeQuestion() {
+
+
+
+}
+
+
+
+
 
 
 
